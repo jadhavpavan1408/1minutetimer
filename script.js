e0ed02faa9cbf3526cf693daffec6b17b02dc8a9
Pavan Jadhav <145537328+jadhavpavan1408@users.noreply.github.com>
2023-11-06 11:27:21 +0530

    25 minute timer

---
+++ script.js
@@ -0,0 +1,45 @@
+var min = 0;
+      var sec = 0;
+      var count = 0;
+      var timer = false;
+      function StartClick() {
+        timer = true;
+        StopWatch();
+      }
+      function StopClick() {
+        timer = false;
+      }
+      function ResetClick() {
+        timer = false;
+        alert("You have Reseted all");
+        document.getElementById("min").innerHTML = "00";
+        document.getElementById("sec").innerHTML = "00";
+        document.getElementById("count").innerHTML = "00";
+        document.querySelector("h1").innerHTML = "";
+        document.querySelector("h2").innerHTML = "";
+      }
+      function StopWatch() {
+        if (timer == true) {
+          count = count + 1;
+          if (count == 100) {
+            sec = sec + 1;
+            count = 0;
+          }
+          if (sec == 60) {
+            min = min + 1;
+            sec = 0;
+          }
+          if (min == 25) {
+            timer = false;
+            document.getElementById("count").innerHTML = "00";
+            document.getElementById("sec").innerHTML = "00";
+            document.querySelector("h1").innerHTML = "25 minute is hover";
+            alert("25 minute is hover....");
+            document.querySelector("h2").style.display = "block";
+          }
+          document.getElementById("count").innerHTML = count;
+          document.getElementById("sec").innerHTML = sec;
+          document.getElementById("min").innerHTML = min;
+          setTimeout("StopWatch()", 10);
+        }
+      }
